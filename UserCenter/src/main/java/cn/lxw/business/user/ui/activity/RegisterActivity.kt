8eb159e116de627f933ddff3a8caf1f18b0b4b9d
= <china2021@126.com>
2018-06-08 17:53:37 +0800

    动态设置 库与 应用程序的切换，新建用户中心的模块

---
+++ UserCenter/src/main/java/cn/lxw/business/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,21 @@
+package cn.lxw.business.user.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import cn.lxw.business.R
+
+/**
+ ***************************************************
+ * 猿代码：Lxw
+ * Email： China2021@126.com
+ * 时间轴：2018年06月08日 17:38
+ ***************************************************
+ * 备注：
+ * 功能描述：
+ */
+class RegisterActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
